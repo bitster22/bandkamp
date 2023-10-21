f4567bb4d3163bc132c80f99682f8222157aed60
bitster22 <57819533+bitster22@users.noreply.github.com>
2023-10-21 01:53:14 -0300

    feat: Tarefa 1 | Projeto Legado finalizada

--- albums/serializers.py
+++ albums/serializers.py
@@ -3,11 +3,17 @@ from .models import Album
 from users.serializers import UserSerializer
 
 
-class AlbumSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(max_length=255)
-    year = serializers.IntegerField()
+class AlbumSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Album
+        fields = "__all__"
+
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(max_length=255)
+    # year = serializers.IntegerField()
+    # user = UserSerializer(read_only=True)
 
     def create(self, validated_data):
         return Album.objects.create(**validated_data)
